c61c97d161138c7a0204e2f7e4e43da438de9758
Kyle <112932317+BrokeOrpheus@users.noreply.github.com>
2023-03-16 02:32:39 -0400

    Line chart added

--- src/pages/Pair.js
+++ src/pages/Pair.js
@@ -3,6 +3,7 @@ import { checkStatus, json } from '../utils';
 import { Dropdown } from '../components/Table.js';
 import { FaChevronRight } from 'react-icons/fa';
 import './Pair.css';
+import { Chart } from 'chart.js/auto';
 
 class Pair extends React.Component {
   constructor(props) {
@@ -20,11 +21,15 @@ class Pair extends React.Component {
     this.changeOutput = this.changeOutput.bind(this);
     this.changeAmount = this.changeAmount.bind(this);
     this.submit = this.submit.bind(this);
+    this.fetchHistory = this.fetchHistory.bind(this);
+    this.buildChart = this.buildChart.bind(this);
+    this.chartRef = React.createRef();
   }
 
   componentDidMount() {
     this.fetchRates();
     this.fetchNames();
+    this.fetchHistory();
   }
 
   fetchRates() {
@@ -53,6 +58,53 @@ class Pair extends React.Component {
     .catch(error => console.log(error));
   }
 
+  fetchHistory () {
+    let { base, outputCurr } = this.state;
+    let startDate = new Date(Date.now() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+
+    fetch(`https://api.frankfurter.app/${startDate}..?from=${base}&to=${outputCurr}`)
+    .then(checkStatus)
+    .then(json)
+    .then(data => {
+      if(data.error) {
+        throw new Error(data.error);
+      }
+
+      let dates = Object.keys(data.rates);
+      let rates = Object.values(data.rates).map(rate => rate[outputCurr]);
+      let head = `${base}/${outputCurr}`;
+
+      this.buildChart(dates, rates, head);
+    })
+    .catch(error => console.log(error));
+  }
+
+  buildChart (dates, rates, head) {
+    let chartRef = this.chartRef.current.getContext('2d');
+
+    if (typeof this.chart !== 'undefined') {
+      this.chart.destroy();
+    }
+
+    this.chart = new Chart(chartRef, {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [
+          {
+            label: head,
+            data: rates,
+            fill: false,
+            tension: 0
+          }
+        ]
+      },
+      options: {
+        responsive: true
+      }
+    })
+  }
+
 
   changeBase (event) {
     this.setState({ base: event.target.getAttribute('name') });
@@ -69,6 +121,7 @@ class Pair extends React.Component {
   submit (event) {
     event.preventDefault();
     this.fetchRates();
+    this.fetchHistory();
   }
 
 
@@ -115,6 +168,11 @@ class Pair extends React.Component {
             </div>
           </div>
           
+        </div>
+        <div id='canvasContainer' className='row justify-content-center mt-5'>
+          <div className='col-12 col-md-10'>
+            <canvas ref={this.chartRef} />
+          </div>
         </div>
       </div>
       
